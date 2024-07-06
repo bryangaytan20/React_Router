debd70864276761a43911987d1ec0553633d3235
bryangaytan20 <bryangaytan20@gmail.com>
2024-07-05 22:53:02 -0600

    Se termino el proyecto

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Sobre from "./pages/Sobre"
 import Page404 from './pages/Pages404';
 import Header from './components/Header';
 import Post from './pages/Post';
+import Categoria from './pages/Categoria';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -14,10 +15,11 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/sobre' element={<Sobre/>}/>
+        <Route path='/' element={<Home/>} />
+        <Route path='/sobre' element={<Sobre/>} />
         <Route path='/posts/:id' element={<Post />} />
-        <Route path='*' element={<Page404/>}/>
+        <Route path='/categoria/:id/*' element={<Categoria />} />
+        <Route path='*' element={<Page404/>} />
       </Routes>
     </Router>
   );
